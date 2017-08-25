eda7ff9c7d2da743926cb5c4af9e82b2509d0eaf
dell <sonia.gupta@franciscanecare.net>
2017-08-25 17:36:25 +0530

    project as on 25 aug 2017

--- src/main/java/pageObjects/transactionReportPageObjects/MonthlyConsolidatedReport.java
+++ src/main/java/pageObjects/transactionReportPageObjects/MonthlyConsolidatedReport.java
@@ -8,6 +8,7 @@ import utilities.GenericReports;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class MonthlyConsolidatedReport extends GenericReports {
     @FindBy(linkText = "Monthly Consolidated Report")WebElement link;
@@ -46,5 +47,14 @@ public class MonthlyConsolidatedReport extends GenericReports {
         close= readFile.getElement(f1, "chequebankclose");
         message= readFile.getElement(f1, "chequebankmessage");
         verify.verifyValidationOnMultiSelect(chqbank, clear, close, message, readFile.readProperty(file, "chequebank"));
+    }
+
+    private void getCollectionList(){
+        HashMap<String, Integer>radioList= new HashMap<String, Integer>();
+
+    }
+
+    public void clickCollectionBase(String value) throws IOException {
+        ehandler.clickRadioButton(readFile.getElements(f1, "radiobuttongroup"), value);
     }
 }
